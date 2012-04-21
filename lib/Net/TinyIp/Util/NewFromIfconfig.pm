0b19fe0292976c3ff6794c3479f3ca705d48cd33
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2012-04-22 01:05:10 +0900

    INIT ver.

--- lib/Net/TinyIp/Util/NewFromIfconfig.pm
+++ lib/Net/TinyIp/Util/NewFromIfconfig.pm
@@ -1,22 +1,46 @@
 package Net::TinyIp::Util::NewFromIfconfig;
 use strict;
 use warnings;
-our $VERSION = '0.01';
+use base "Net::TinyIp::Util";
+
+our $VERSION = "0.01";
+
+sub import_methods { qw( new_from_ifconfig ) }
+
+sub new_from_ifconfig {
+    my $class = shift;
+    my %param = @_;
+
+    if ( my $if = $param{if} ) {
+        $param{config} = join q{}, `ifconfig $if`;
+    }
+
+    my( $line ) = grep { m{\A \s+ inet[ ] }msx }
+                  split m{\n}, $param{config};
+
+    my( $host, $network ) = $line =~ m{\A \s+ inet[ ](.+?) [ ] netmask[ ](.+?) [ ] }msx;
+
+    $host    = Net::TinyIp::Address::v4->from_string( $host );
+    $network = Net::TinyIp::Address::v4->from_hex( $network );
+
+    return bless { host => $host, network => $network }, $class;
+}
 
 1;
 __END__
 
 =head1 NAME
 
-Net::TinyIp::Util::NewFromIfconfig -
+Net::TinyIp::Util::NewFromIfconfig - new IPv4 from ifconfig command
 
 =head1 SYNOPSIS
 
-  use Net::TinyIp::Util::NewFromIfconfig;
+  use Net::TinyIp qw( new_from_ifconfig );
+  my $ip = Net::TinyIp->new_from_ifconfig( if => "en0" );
 
 =head1 DESCRIPTION
 
-Net::TinyIp::Util::NewFromIfconfig is
+Net::TinyIp::Util::NewFromIfconfig is a util module.
 
 =head1 AUTHOR
 
@@ -30,3 +54,4 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+
